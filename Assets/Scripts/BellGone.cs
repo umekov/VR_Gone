c967bac5bf07c28fff08f8825fa07a57b9e4d540
umekov <umekov.virtual@gmail.com>
2019-01-12 01:13:16 +0900

    VRGone

---
+++ Assets/Scripts/BellGone.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BellGone : MonoBehaviour
+{
+
+    public AudioSource bell_gone_sound;
+    public AudioClip bell_sound;
+    private int bell_count;
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Stick")
+        {
+            PlaySound();
+        }
+    }
+
+    public void PlaySound()
+    {
+        bell_gone_sound.Play();
+        bell_count += 1;
+    }
+}
