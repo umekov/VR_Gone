6edc371b856e44854c966301b26df7453f305127
umekov <umekov.virtual@gmail.com>
2019-03-02 00:47:37 +0900

    WIP DeleteHumanDesires

--- Assets/Scripts/BellGone.cs
+++ Assets/Scripts/BellGone.cs
@@ -6,17 +6,25 @@ public class BellGone : MonoBehaviour
     public AudioSource bell_gone_sound;
     public AudioClip bell_sound;
     private int bell_count;
+
+    // 鐘にオブジェクトが衝突した際に実行される
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Stick")
         {
-            PlaySound();
+            bell_gone_sound.Play();
+            BellCount();            
         }
     }
 
-    public void PlaySound()
-    {
-        bell_gone_sound.Play();
+    void BellCount(){
         bell_count += 1;
+        if(bell_count == 108){
+            DeleteHumanDesires();
+        }
+    }
+
+    void DeleteHumanDesires() {
+        // TODO: bell_countが108になったときの処理を考える
     }
 }
